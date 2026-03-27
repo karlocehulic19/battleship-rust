ab559edf7543a94e4c6c0c098f385f30327771d7
Karlo Cehulic <karlocehlic@gmail.com>
2026-03-27 21:38:38 +0100

    Add basic method for placing ships

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 const SIZE: usize = 10;
 
 fn main() {
-    let player1 = Player {
+    let mut player1 = Player {
         name: String::from("Joe"),
         board: [[0; SIZE]; SIZE],
     };
@@ -10,6 +10,7 @@ fn main() {
         board: [[0; SIZE]; SIZE],
     };
 
+    player1.place_ship(1, 1, 3);
     println!("Player {}, with board {:?}", player1.name, player1.board);
     println!("Player {}, with board {:?}", player2.name, player2.board);
 }
@@ -17,6 +18,15 @@ fn main() {
 struct Player {
     name: String,
     board: [[u32; SIZE]; SIZE],
+}
+
+impl Player {
+    fn place_ship(&mut self, row: usize, col: usize, len: usize) {
+        let end_col = col + len;
+        for c in col..end_col {
+            self.board[row][c] = 1;
+        }
+    }
 }
 
 fn attack(row: usize, col: usize, board: &mut [[u32; SIZE]; SIZE]) {
