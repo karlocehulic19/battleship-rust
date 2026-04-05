02a77a14ec9a3da2a792fa536cace8f22ef2999a
Karlo Cehulic <karlocehlic@gmail.com>
2026-04-06 00:51:23 +0200

    refactor: use recommended App impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
@@ -6,39 +9,69 @@ use ratatui::{
     text::{Line, Text, ToLine},
     widgets::{Block, Borders, Paragraph, Widget},
 };
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    ratatui::run(app)?;
-    Ok(())
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+#[derive(Debug, Default)]
+struct App {
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events();
         }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
     }
 }
 
-fn render(frame: &mut Frame) {
-    let b = Block::default()
-        .borders(Borders::ALL)
-        .title(Line::from("Tetris").centered());
-    let p = render_playing_box(b);
-    frame.render_widget(p, frame.area());
-}
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .title(Line::from("Tetris").centered());
 
-fn render_playing_box(block: Block) -> Paragraph<'_> {
-    let horizontal_border_block = Line::from("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩".white());
-    let vertical_border_block = Line::from("🟩                    🟩".white());
-    let mut vector_box: Vec<Line<'_>> = Vec::new();
+        let horizontal_border_block = Line::from("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩".white());
+        let vertical_border_block = Line::from("🟩                    🟩".white());
+        let mut vector_box: Vec<Line<'_>> = Vec::new();
 
-    let mut horizontal_vector = vec![vertical_border_block; 10];
-    vector_box.push(horizontal_border_block.clone());
-    vector_box.append(&mut horizontal_vector);
-    vector_box.push(horizontal_border_block.clone());
+        let mut horizontal_vector = vec![vertical_border_block; 10];
+        vector_box.push(horizontal_border_block.clone());
+        vector_box.append(&mut horizontal_vector);
+        vector_box.push(horizontal_border_block.clone());
 
-    let playing_box = Text::from(vector_box);
-    return Paragraph::new(playing_box).centered().block(block);
+        let playing_box = Text::from(vector_box);
+        Paragraph::new(playing_box)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
